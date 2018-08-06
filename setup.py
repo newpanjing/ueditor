bc5ff9bea382183a2753c9ab38c3bdd0b0416e70
newpanjing <newpanjing@163.com>
2018-08-06 15:29:12 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ueditor',
+    version='0.1.5',
+    packages=['ueditor','ueditor.templates.ueditor','ueditor.static.ueditor','ueditor.static.ueditor.ueditor'],
+    url='',
+    license='',
+    author='panjing',
+    author_email='newpanjing@icloud.com',
+    description='',
+    install_requires=['django'],
+    zip_safe=True
+)
